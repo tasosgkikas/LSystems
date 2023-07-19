c99a5715e1b4739a0bbbb7f368d631e7f96bade4
Tasos Gkikas <anast.gkikas@gmail.com>
2023-07-19 14:43:45 +0300

    LSystem class in file

--- src/FractalPlant.java
+++ src/FractalPlant.java
@@ -3,6 +3,47 @@ import javax.swing.event.ChangeEvent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.geom.Ellipse2D;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class LSystem {
+    Map<String, String> rule;
+
+    LSystem(String[] variables, String[] mappings) {
+        if (variables.length != mappings.length)
+            throw new IllegalArgumentException("Argument arrays must have the same length.");
+
+        rule = new HashMap<>();
+        for (int i = 0; i < variables.length; i++)
+            rule.put(variables[i], mappings[i]);
+    }
+
+    String produce(String axiom, int iterations) {
+        if (iterations == 0) return axiom;
+
+        System.out.println(axiom);
+
+        List<String> tokens = Arrays.asList(axiom.split(""));
+        tokens.replaceAll((String token) -> {
+            String substitute = rule.get(token);
+            return substitute != null ? substitute : token;
+        });
+        axiom = String.join("", tokens);
+
+        return produce(axiom, iterations - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+            (new LSystem(
+                new String[]{"X", "F"},
+                new String[]{"F+[[X]-X]-F[-FX]+X", "FF"}
+            )).produce("X", 6)
+        );
+    }
+}
 
 public class FractalPlant extends JFrame {
     private static class Control { // controls παραμέτρων
