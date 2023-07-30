02f9b2409e67560b069e86f14dac7a1ee70d9b0e
Tasos Gkikas <anast.gkikas@gmail.com>
2023-07-30 15:33:04 +0300

    DottedLine ok

---
+++ src/DottedLine.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+class DottedLine extends Drawer {
+    public DottedLine() {
+        super("F", new LSystem(
+            new char[]{'F', 'X'},
+            new String[]{"FXF", "XFX"}
+        ));
+    }
+
+    @Override
+    protected void paintComponent(Graphics2D canvas) {
+        canvas.translate(0, getHeight() / 2);
+        double step = getWidth() / (Math.pow(3, getITERATIONS()));
+        for (char c : PRODUCT.toCharArray()) {
+            if (c == 'F')
+                canvas.drawLine(0, 0, (int) step, 0);
+            canvas.translate(step, 0);
+        }
+    }
+}
